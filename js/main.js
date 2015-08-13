2f1fbcaef2c02b0fb6f35ded927dbb2fbce175ff
Benjamin André <notbenj@gmail.com>
2015-08-13 14:09:36 +0200

    Jekyll integration and blog section

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var initEasterEgg = function() {
+    var easter_egg = new Konami();
+    easter_egg.load('http://gop-labs.xyz');
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    initEasterEgg();
+});
